fec2ab6c8da6c9514e9944a51a4a78fa8c81c168
stepanet <stepanet@gmail.com>
2019-09-22 11:19:49 +0300

    для потомков

--- MultiplyTraine/ViewController.swift
+++ MultiplyTraine/ViewController.swift
@@ -3,7 +3,7 @@
 //  MultiplyTraine
 //
 //  Created by Денис Сидоренко on 20/09/2019.
-//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//  Copyright © 2019 Денис Сидоренко + Jack Sp@rrow. All rights reserved.
 //
 
 import UIKit
